49d9eed23c3754b1a305fb2fc3adbb472a38b246
Gaurav <gkgarry911@gmail.com>
2019-10-28 01:02:42 +0530

    first Ui

--- Tokri-Bazzar/src/app/app-routing.module.ts
+++ Tokri-Bazzar/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { EmployeeComponent } from './controllers/employee/employee.component';
+import { AdminComponent } from './controllers/admin/admin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+     {path : "" , redirectTo : "/index" , pathMatch : "full"},
+     {path : "index", component: IndexComponent},
+     { path : "admin" , component : AdminComponent },
+     { path : "employee", component : EmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tokri-Bazzar/src/app/app.module.ts
+++ Tokri-Bazzar/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
+import { AdminComponent } from './controllers/admin/admin.component';
+import { EmployeeComponent } from './controllers/employee/employee.component';
+import { NevbarComponent } from './nevbar/nevbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AdminComponent,
+    EmployeeComponent,
+    NevbarComponent
   ],
   imports: [
     BrowserModule,
